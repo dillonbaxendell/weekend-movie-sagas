4a9b5d8dfb10f5d62914b62f4c61135b15c19131
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-20 12:55:43 -0500

    card styling on MovieList and Details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,28 @@
+//Styling
+import "./Details.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+//React imports
 import { useSelector } from 'react-redux';
 import {useHistory} from 'react-router';
 
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 500,
+    },
+    media: {
+      height: 650,
+    },
+  });
+
 function Details() {
+    const classes = useStyles();
 
     const movie = useSelector( store => store.details);
     const genres = useSelector( store => store.genres);
@@ -9,11 +30,20 @@ function Details() {
     const history = useHistory();
 
     return (
-        <>
-            <div key={movie.id} >
-                <h3>{movie.title}</h3>
-                <img src={movie.poster} alt={movie.title} />
-            </div>
+    <main>
+    <Grid container direction="column" alignItems="center" justify="center">
+        <div className="grid">
+        <Grid item >
+        <Card className={classes.root}>
+            <CardActionArea>
+            <img className="image"
+                 src={movie.poster} 
+                 alt={movie.title} 
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {movie.title}
+                </Typography>
             <div>
                 <h4>Genres:</h4>
                 {genres.map(genre => {
@@ -24,14 +54,19 @@ function Details() {
                     );
                 })}
             </div>
-            <div>
-                <p>{movie.description}</p>
-            </div>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {movie.description}
+            </Typography>
+            </CardContent>
             <div>
                 <button onClick={() => history.push('/')}>Back</button>
             </div>
-
-        </>
+            </CardActionArea>
+        </Card>
+        </Grid>
+        </div>
+        </Grid>
+        </main>
     )
 }
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,31 @@
+//STYLING
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 //Other imports
 import {useHistory} from 'react-router';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 350,
+      width: 250,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
 
 function MovieList() {
     //access history and dispatch
@@ -10,6 +33,7 @@ function MovieList() {
     const dispatch = useDispatch();
     //set movies as the movies reducer from Redux
     const movies = useSelector(store => store.movies);
+    const classes = useStyles();
 
     //On page load, run these functions
     useEffect(() => {
@@ -40,20 +64,28 @@ function MovieList() {
     }
 
     return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
+    <>
+        <Grid container direction="column" alignItems="center" justify="center" className={classes.root}>
+            <Grid className="movies">
+
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title} onClick={(event) => {goToDetails(movie)}}/>
-                        </div>
+                        <Card variant="outlined" className="movies">
+                        <CardActionArea>
+                        <CardContent className={classes.paper} key={movie.id} >
+                            <Typography variant="h6">
+                                {movie.title}
+                            </Typography>
+                            <img className="image" src={movie.poster} alt={movie.title} onClick={(event) => {goToDetails(movie)}}/>
+                        </CardContent>
+                        </CardActionArea>
+                        </Card>
                     );
                 })}
-            </section>
-        </main>
 
+            </Grid>
+        </Grid>
+</>
     );
 }
 
